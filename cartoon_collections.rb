0da2f13fd3c814fdb177e1c3c0e84c447688a615
Javante Nelson <javantenelson@gmail.com>
2020-03-26 02:51:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ team=array.map {|rings| rings.capitalize}
 team.map {|ring| "#{ring}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |words| words.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
