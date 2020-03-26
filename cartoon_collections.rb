0ab5220111b6789ba6b3c7e55739c54d5fc07af7
Javante Nelson <javantenelson@gmail.com>
2020-03-26 01:22:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  dwarfs=[]
+  names.each_with_index(1) |name, index| puts "#{index} #{name}"
 end
 
 def summon_captain_planet# code an argument here
